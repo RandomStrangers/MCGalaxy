88ab245e531336c3a886019766dfcd33a8263d39
June Solis <110739593+RandomStrangers@users.noreply.github.com>
2026-02-06 19:33:39 -0500

    Delete MCGalaxy/nas directory

--- MCGalaxy/nas/GlobalSupressions.cs
+++
@@ -1,2 +0,0 @@
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0059:Unnecessary assignment of a value", Justification = "<Pending>")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0079:Unnecessary suppression", Justification = "<Pending>")]
